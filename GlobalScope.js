be078b0647af50ba2e860e07531e81e5c7b7f58c
frauq3333 <franciscoignacio.quintana@alumnos.ulagos.cl>
2024-09-21 14:57:52 -0300

    Se sube un GlobalScope como apunte

---
+++ GlobalScope.js
@@ -0,0 +1,13 @@
+var comic = 'X-Men'
+
+function comprar () {
+    console.log('Compraste el comic ${comic}')
+
+}
+
+function leer (){
+    console.log('Esta leyendo ${comic}')
+}
+
+comprar()
+leer()

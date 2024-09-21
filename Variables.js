f63dd30dc0fa3f6156666c4b24c09840ed4428a2
frauq3333 <franciscoignacio.quintana@alumnos.ulagos.cl>
2024-09-21 14:58:52 -0300

    Se sube un apunte de variables

---
+++ Variables.js
@@ -0,0 +1,20 @@
+var estaturas = 1.80
+let peso = 82
+
+const nombre = "Francisco"
+
+var edad
+let ciudad
+
+edad = 20
+ciudad = "Castro"
+
+const apellido = "Quintana"
+
+const nombrecompleto = nombre + "" + apellido
+console.log(nombrecompleto);
+
+let docente = "Victor", age = 29, asignaturas = ['Prog.A','Programacion En Aplicaciones Moviles'];
+
+console.log(n);
+var n = 10;

c3fb64cf4dbde14e5972f4ad93ca5f60c18a2c72
frauq3333 <franciscoignacio.quintana@alumnos.ulagos.cl>
2024-10-16 16:44:54 -0300

    Se sube Hola Mundo usado en App Movil

---
+++ HolaMundo.js.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>¡Hola Mundo!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0', // Color de fondo
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333', // Color del texto
+  },
+});
